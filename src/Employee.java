b2220c86eac52b314277841791ea2b2a5949ec21
D4_62972_Udhav <udhavpatil0806@gmail.com>
2024-04-17 18:25:56 +0530

    method overloading

--- src/Employee.java
+++ src/Employee.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class Employee {
+public class Employee {
+    private int id;
+    private String name;
+
+    public Employee(int id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    // compileTime Polymorphism
+    public int add(int a, int b){
+        return a+b;
+    }
+
+    public int add(int a, int b, int c){
+        return a+b+c;
+    }
+
+    public int add(int a, int b, int c, int d){
+        return a+b+c+d;
+    }
+
+    public void display(){
+        System.out.println(id+" "+name);
+    }
 }
